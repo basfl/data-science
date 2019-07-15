23ede9639be065d58321c59cef16fb7d1e524e2c
unknown <jupiter85838@yahoo.com>
2019-07-15 14:13:14 -0400

    fixing error in total_minutes_by_account and using numpy to calculate some stats

--- csv/exploring_student_engagement.py
+++ csv/exploring_student_engagement.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from exploring_data import paid_engagement_in_first_week
+import numpy as np
+
 """
 Create a dictionary of engagement grouped by student.
 The keys are account keys, and the values are lists of engagement records.
@@ -12,7 +14,6 @@ for engagement_record in paid_engagement_in_first_week:
     # break;
 
 
-
 """
 Create a dictionary with the total minutes each student spent in the classroom during the first week.
 The keys are account keys, and the values are numbers (total minutes)
@@ -22,6 +23,15 @@ total_minutes_by_account = {}
 for account_key, engagement_for_student in engagement_by_account.items():
     total_minutes = 0
     for engagement_record in engagement_for_student:
-        total_minutes+=engagement_record['total_minutes_visited']
-        total_minutes_by_account['account_key']=total_minutes
+        total_minutes += engagement_record['total_minutes_visited']
+        total_minutes_by_account[account_key] = total_minutes
 
+
+# Summarize the data about minutes spent in the classroom
+# need to convert the defaultdict values to list for numpy
+total_minutes = list(total_minutes_by_account.values())
+
+print(f'Mean: {np.mean(total_minutes)}')
+print(f'Standard deviation: {np.std(total_minutes)}')
+print(f'Minimum: {np.min(total_minutes)}')
+print(f'Maximum: {np.max(total_minutes)}')
